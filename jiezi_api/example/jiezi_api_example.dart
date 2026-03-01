b110552cf647e338d05625ac5e6cf9ae572baf94
arcticfox <hellocpp2x@gmail.com>
2026-03-01 22:46:34 +0800

    feat: add jiezi_api, jiezi_web and mustard-seed

---
+++ jiezi_api/example/jiezi_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:jiezi_api/jiezi_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ mustard-seed/android/app/src/main/kotlin/xyz/bcl/mustard_seed/MainActivity.kt
@@ -0,0 +1,5 @@
+package xyz.bcl.mustard_seed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
